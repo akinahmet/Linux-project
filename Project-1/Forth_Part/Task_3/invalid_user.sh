3505d285010f1aadc39d28d02be98d2f628be362
AKN4 <ahmtakn@protonmail.com>
2022-06-30 10:40:11 +0200

    part4 task3

--- Project-1/Forth_Part/Task_3/invalid_user.sh
+++ Project-1/Forth_Part/Task_3/invalid_user.sh
@@ -2,3 +2,5 @@
 echo "invalid user list"
 awk '/Invalid user/' auth.log | awk -F"]: " '{print $2}' | awk -F" " '{print $3}' | sort | uniq -c
 
+# alternative solutions
+# grep -Eio "input_userauth_request: invalid user .+ " auth.log | awk '{print $4}' | sort | uniq -c > invalid_user.sh
