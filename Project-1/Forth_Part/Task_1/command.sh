0f9f05af34da01896b1108636ba174a8ebe20bcf
AKN4 <ahmtakn@protonmail.com>
2022-06-30 10:15:42 +0200

    part4 task1

--- Project-1/Forth_Part/Task_1/command.sh
+++ Project-1/Forth_Part/Task_1/command.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 privatIP=$(sed '20!d' info.json | awk -F\" '{print $4}')
-sed "s/ec2-private_ip/$privatIP/" terraform.tf > terraform_new.tf
+sed "s/ec2-private_ip/$privatIP/" terraform.tf > terraform_new.tf  # s ile al, private_ip'yi bul sonra $privatIP'de degistir 
+
+# alternative solutions
+# cat info.json | grep "PrivateIPAddress" | head -1 | awk -F\" '{print $4}'
+# sed i flag ile yerinde yazilabilir
+# sed -i "s/ec2-private_ip/$privatIP/" terraform.tf  (mevcut dosyanin üzerine yazdik)
