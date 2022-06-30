3ddda5cf0d54f495e65b749b90fcf32a9387225f
AKN4 <ahmtakn@protonmail.com>
2022-06-30 09:27:26 +0200

    part1

--- Project-1/First_Part/script.sh
+++ Project-1/First_Part/script.sh
@@ -4,6 +4,11 @@
 # tail event_history.csv 
 
 content=$(awk -F, '/serdar/&&/TerminateInstance/ {print $10}' event_history.csv | awk -F: '{print $2}' | awk -F\" '{print $3}' | sort | uniq) 
+# Alternative solutions
+# cat event_history.csv | grep -i serdar | grep -i TerminateInstance | grep -Eo "i-[a-zA-Z0-9]{17}" | sort | uniq
+# grep -Eo (o:only)
+# grep ile yapilan sonuc daha dogru
+
 
 echo "Instance ID's terminated by user 'serdar'" >> result.txt
 echo "------------------" >> result.txt
